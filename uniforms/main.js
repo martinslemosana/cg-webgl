18f133d79eb65ba95076771778fbf399facada30
Ana Lemos <martins.ana@unifesp.br>
2024-10-28 14:11:25 -0300

    mouse-keyboard-interaction

--- uniforms/main.js
+++ uniforms/main.js
@@ -17,60 +17,113 @@ function main(){
     gl.useProgram(program);
   
     const positionBuffer = gl.createBuffer();
-    const colorBuffer = gl.createBuffer();
   
     const positionLocation = gl.getAttribLocation(program, `position`);
     gl.enableVertexAttribArray(positionLocation);
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
     gl.vertexAttribPointer(positionLocation, 2, gl.FLOAT, false, 0, 0);
   
-    const colorLocation = gl.getAttribLocation(program, `color`);
-    gl.enableVertexAttribArray(colorLocation);
-    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-    gl.vertexAttribPointer(colorLocation, 3, gl.FLOAT, false, 0, 0);
-  
+    const uniformLocations = {
+        matrix: gl.getUniformLocation(program, `matrix`),
+        color: gl.getUniformLocation(program, `color`),
+    };
+    let matrix = mat4.fromValues(
+        2/canvas.width, 0, 0, 0,
+        0, -2/canvas.height, 0, 0,
+        0, 0, 0, 0,
+        -1, 1, 0, 1
+    );
+
     gl.clearColor(1.0, 1.0, 1.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
     
+    gl.viewport(0, 0, canvas.width, canvas.height);
+    
     canvas.addEventListener("mousedown",mouseClick,false);
   
+    let positionVector = [canvas.width/2,canvas.height/2];
     function mouseClick(event){
-      console.log(event);
+      positionVector = [event.clientX,event.clientY];
+      drawPoint();
     }
   
     const bodyElement = document.querySelector("body");
     bodyElement.addEventListener("keydown",keyDown,false);
-
-    function keyDown(event){
-      console.log(event);
-    }
-}
   
-function createShader(gl, type, source) {
+    let colorVector = [0.0,0.0,0.0];
+    function keyDown(event){
+      switch(event.key){
+        case "0":
+          colorVector = [0.0,0.0,0.0];
+          break;
+        case "1":
+          colorVector = [1.0,0.0,0.0];
+          break;
+        case "2":
+          colorVector = [0.0,1.0,0.0];
+          break;
+        case "3":
+          colorVector = [0.0,0.0,1.0];
+          break;
+        case "4":
+          colorVector = [1.0,1.0,0.0];
+          break;
+        case "5":
+          colorVector = [0.0,1.0,1.0];
+          break;
+        case "6":
+          colorVector = [1.0,0.0,1.0];
+          break;
+        case "7":
+          colorVector = [1.0,0.5,0.5];
+          break;
+        case "8":
+          colorVector = [0.5,1.0,0.5];
+          break;
+        case "9":
+          colorVector = [0.5,0.5,1.0];
+          break;
+      }
+      drawPoint();
+    }
+  
+    function drawPoint(){
+      gl.clear(gl.COLOR_BUFFER_BIT);
+      gl.bindBuffer(gl.ARRAY_BUFFER,positionBuffer);
+      gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positionVector), gl.STATIC_DRAW);
+      gl.uniformMatrix4fv(uniformLocations.matrix, false, matrix);
+      gl.uniform3fv(uniformLocations.color,colorVector);
+      gl.drawArrays(gl.POINTS, 0, 1);
+    }
+  
+    drawPoint();
+  }
+  
+  function createShader(gl, type, source) {
     var shader = gl.createShader(type);
     gl.shaderSource(shader, source);
     gl.compileShader(shader);
     var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
     if (success) {
-        return shader;
+      return shader;
     }
-
+  
     console.log(gl.getShaderInfoLog(shader));
     gl.deleteShader(shader);
-}
+  }
   
-function createProgram(gl, vertexShader, fragmentShader) {
+  function createProgram(gl, vertexShader, fragmentShader) {
     var program = gl.createProgram();
     gl.attachShader(program, vertexShader);
     gl.attachShader(program, fragmentShader);
     gl.linkProgram(program);
     var success = gl.getProgramParameter(program, gl.LINK_STATUS);
     if (success) {
-        return program;
+      return program;
     }
-
+  
     console.log(gl.getProgramInfoLog(program));
     gl.deleteProgram(program);
-}
+  }
   
-main();
+  main();
